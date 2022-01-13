cf2b0a881dae90788a50cde0d34e98b02c36d9f5
Alovya Chowdhury <alovya.chowdhury@gmail.com>
2022-01-13 06:15:55 +0000

    add dict storage of velocity

--- cases/25-blocks/plot-experimental-data.py
+++ cases/25-blocks/plot-experimental-data.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import collections
 import pandas            as pd
 import numpy             as np
 import matplotlib.pyplot as plt
@@ -7,7 +8,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 class CrossSectionData:
-    def __init__(self):
+    def __init__(
+            self,
+            epsilons
+        ):
         header = []
         
         with open("25-blocks.start", 'r') as fp:
@@ -27,9 +31,17 @@ class CrossSectionData:
         
         self.xmin       = xmin
         self.cellsize   = cellsize
+        self.epsilons   = epsilons
         self.i_range    = [ _ for _ in range(col_start,col_end) ]
         self.intervals  = [ _ for _ in range(4,9) ]
         self.velocities = {}
+        
+        for interval in self.intervals:        
+            if interval not in self.velocities:
+                self.velocities[interval] = {}
+                for key in (*self.epsilons, "lisflood"):
+                    if key not in self.velocities[interval]:
+                        self.velocities[interval][key] = {}
         
         y_range = 0.2 - ymin
         
@@ -39,64 +51,68 @@ class CrossSectionData:
         # +6 to skip the 6 header lines
         self.slice_row = nrows - row + 6
 
-    def run_solver(
+    def run_adaptive(
             self,
             epsilon
         ):
-        with open("25-blocks.par", 'w') as fp:
-            params = (
-                "test_case   0\n" +
-                "max_ref_lvl 11\n" +
-                "min_dt      1\n" +
-                "respath     .\\results\n" +
-                "epsilon     %s\n" +
-                "fpfric      0.01\n" +
-                "rasterroot  25-blocks\n" +
-                "tol_h       1e-3\n" +
-                "tol_q       0\n" +
-                "tol_s       1e-9\n" +
-                "g           9.80665\n" +
-                "saveint     1\n" +
-                "sim_time    10\n" +
-                "raster_out  on\n" +
-                "solver      hw\n" +
-                "wall_height 2.5"
-            ) % epsilon
+            with open("25-blocks.par", 'w') as fp:
+                params = (
+                    "test_case   0\n" +
+                    "max_ref_lvl 11\n" +
+                    "min_dt      1\n" +
+                    "respath     .\\results\n" +
+                    "epsilon     %s\n" +
+                    "fpfric      0.01\n" +
+                    "rasterroot  25-blocks\n" +
+                    "tol_h       1e-3\n" +
+                    "tol_q       0\n" +
+                    "tol_s       1e-9\n" +
+                    "g           9.80665\n" +
+                    "saveint     1\n" +
+                    "sim_time    10\n" +
+                    "raster_out  on\n" +
+                    "solver      hw\n" +
+                    "wall_height 2.5"
+                ) % epsilon
+                
+                fp.write(params)
             
-            fp.write(params)
+            subprocess.run( [os.path.join("..", "gpu-mwdg2.exe"), "25-blocks.par"] )
         
-        subprocess.run( [os.path.join("..", "gpu-mwdg2.exe"), "25-blocks.par"] )
-        
-    def extract(
+    def extract_adaptive(
             self,
             epsilon
         ):
             for interval in self.intervals:
                 extract_file = os.path.join("results", "results-" + str(interval) + ".wd")
-                save_file    = "mwdg2-eps-" + str(epsilon) + "-" + str(interval) + ".wd"
                 
-                self.velocities[interval] = np.loadtxt(
+                self.velocities[interval][epsilon] = np.loadtxt(
                     fname=extract_file,
                     skiprows=self.slice_row,
                     max_rows=1,
                     usecols=self.i_range
                 )
+                
+    def run_and_extract_adaptive_all(self):
+        for epsilon in self.epsilons:
+            self.run_adaptive(epsilon)
+            self.extract_adaptive(epsilon)
     
-    def plot(
-            self,
-            epsilon
-        ):
-            for interval in self.intervals:
-                plt.plot( [self.xmin + i * self.cellsize for i in self.i_range], self.velocities[interval] )
-                plt.show()
-                plt.close()
+    def plot(self):
+        fig, ax = plt.subplots()
+        
+        for interval in self.intervals:
+            for config, data in self.velocities[interval].items():
+                if config == "lisflood": continue
+                ax.plot([self.xmin + i * self.cellsize for i in self.i_range], data, label=config)  
             
-    def run_extract_and_plot(
-            self,
-            epsilon
-        ):
-            self.extract(epsilon)
-            self.plot(epsilon)
+            plt.legend()
+            plt.savefig("why-" + str(interval))
+            ax.clear()
+            
+    def run_extract_and_plot_all(self):
+        self.run_and_extract_adaptive_all()
+        self.plot()
     
 if __name__ == "__main__":
-    CrossSectionData().run_extract_and_plot(1e-3)
+    CrossSectionData( [1e-2, 1e-3] ).run_extract_and_plot_all()
