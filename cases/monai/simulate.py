15e39c4c3e455ebf1bee63cc0c46c2002d8a3a6f
Alovya Chowdhury <alovya.chowdhury@gmail.com>
2022-01-14 04:12:07 +0000

    add monai experimental data and labeling in sim script

--- cases/monai/simulate.py
+++ cases/monai/simulate.py
@@ -2,8 +2,14 @@ import os
 import subprocess
 import numpy             as np
 import pandas            as pd
-import matplotlib.pylab  as pylab
 import matplotlib.pyplot as plt
+
+class ExperimentalDataMonai:
+    def __init__(self):
+        experimental_dataframe = pd.read_csv("experimental.txt")
+        
+        self.time       = experimental_dataframe["time"]
+        self.gauge_data = experimental_dataframe["stage1"]
 
 class SimulationMonai:
     def __init__(
@@ -24,7 +30,7 @@ class SimulationMonai:
                             self.results[config][field] = {}
                             
             for epsilon in epsilons:
-                self.run_adaptive(epsilon)
+                #self.run_adaptive(epsilon)
                 
                 time_dataframe = pd.read_csv(self.runtime_file)
                 
@@ -63,35 +69,74 @@ class SimulationMonai:
             
             subprocess.run( [os.path.join("..", "gpu-mwdg2.exe"), "monai.par"] )
             
-    def plot(self):
-        params = {
-            "legend.fontsize" : "xx-large",
-            "axes.labelsize"  : "xx-large",
-            "axes.titlesize"  : "xx-large",
-            "xtick.labelsize" : "xx-large",
-            "ytick.labelsize" : "xx-large"
-        }
-        
-        pylab.rcParams.update(params)
-        
-        fig, ax = plt.subplots()
-        
-        for config in self.configs:
-            if config == "lisflood": continue
-            ax.plot(self.results[config]["simtime"], self.results[config]["gauge_data"], label=config)
-        
-        plt.legend()
-        plt.savefig( os.path.join("results", "stage") )
-        ax.clear()
-        
-        for config in self.configs:
-            if config == "lisflood": continue
-            runtime_ratio = self.results[config]["runtime"] / self.results[0]["runtime"]
-            ax.plot(self.results[config]["simtime"], runtime_ratio, label=config)
-        
-        plt.legend()
-        plt.savefig( os.path.join("results", "runtimes") )
-        ax.clear()
+    def plot(
+            self,
+            exp_data
+        ):
+            my_rc_params = {
+                "legend.fontsize" : "xx-large",
+                "axes.labelsize"  : "xx-large",
+                "axes.titlesize"  : "xx-large",
+                "xtick.labelsize" : "xx-large",
+                "ytick.labelsize" : "xx-large"
+            }
+            
+            plt.rcParams.update(my_rc_params)
+            
+            fig, ax = plt.subplots()
+            
+            for config in self.configs:
+                if config == "lisflood": continue
+                
+                ax.plot(
+                    self.results[config]["simtime"],
+                    self.results[config]["gauge_data"] - 0.13535,
+                    linewidth=2.5,
+                    label=config
+                )
+            
+            ax.scatter(
+                exp_data.time,
+                exp_data.gauge_data,
+                facecolor="None",
+                edgecolor="black",
+                label="Experimental"
+            )
+            
+            ax.set_xlabel(r"$t \, (s)$")
+            ax.set_ylabel(r"Free surface elevation $(m)$")
+            ax.set_xlim( exp_data.time.iloc[0], exp_data.time.iloc[-1] )
+            ax.legend()
+            fig.savefig(os.path.join("results", "stage"), bbox_inches="tight")
+            ax.clear()
+            
+            for config in self.configs:
+                if config == "lisflood": continue
+                
+                runtime_ratio = self.results[config]["runtime"] / self.results[0]["runtime"]
+                
+                ax.plot(
+                    self.results[config]["simtime"],
+                    runtime_ratio,
+                    linewidth=2.5,
+                    label=config
+                )
+            
+            config = self.configs[0]
+            
+            xlim = (
+                ( self.results[config]["simtime"] ).iloc[0],
+                ( self.results[config]["simtime"] ).iloc[-1]
+            )
+            
+            ax.set_xlabel(r"$t \, (s)$")
+            ax.set_ylabel("Speedup ratio GPU-MWDG2/GPU-DG2")
+            ax.set_xlim(xlim)
+            ax.legend()
+            fig.savefig(os.path.join("results", "runtimes"), bbox_inches="tight")
+            ax.clear()
+            
+            plt.close()
         
 if __name__ == "__main__":
-    SimulationMonai( [1e-3, 1e-4, 0] ).plot()
+    SimulationMonai( [1e-3, 1e-4, 0] ).plot( ExperimentalDataMonai() )
