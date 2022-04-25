f8a1e20f0e546699863aafc69280c93c179eef14
cip19aac <alovya.chowdhury@gmail.com>
2022-04-25 16:56:27 +0100

    modify three dam break script to plot map of errors

--- cases/robustness/simulate.py
+++ cases/robustness/simulate.py
@@ -34,6 +34,16 @@ def run_c_prop_tests():
     
     subprocess.run( ["matlab", "-nosplash", "-nodesktop", "-r", "\"main; exit\""] )
     
+def remove_nans_and_infs(array):
+    nan_locations = np.isnan(array)
+    inf_locations = np.isinf(array)
+    
+    removal_locations = np.logical_or(nan_locations, inf_locations)
+    
+    array[removal_locations] = 0
+    
+    return array
+
 class SimulationThreeConesDamBreak:
     def __init__(
         self,
@@ -52,7 +62,7 @@ class SimulationThreeConesDamBreak:
         
         # runs for speedups
         for epsilon in self.epsilons:
-            self.run(
+            self.run_dam_break(
                 solver=solver,
                 epsilon=epsilon
             )
@@ -91,7 +101,7 @@ class SimulationThreeConesDamBreak:
         
         self.X, self.Y = np.meshgrid(x, y)
                 
-    def run(
+    def run_dam_break(
         self,
         solver,
         epsilon
@@ -131,61 +141,63 @@ class SimulationThreeConesDamBreak:
         
         fig, axs = plt.subplots(
             nrows=7,
-            ncols=2,
-            gridspec_kw={"height_ratios" : [size, 1, size, 1, size, 1, size/15]},
-            figsize=(10,10)
+            ncols=1,
+            gridspec_kw={ "height_ratios" : [size, 1, size, 1, size, 1, size/15] },
+            figsize=(8,10)
         )
         
         plt.setp( axs, ylabel=(r"$y \, (m)$"), xlabel=(r"$x \, (m)$") )
         
-        all_h = []
+        all_errors = []
         
-        for epsilon in self.epsilons:
-            for interval in self.intervals:
-                all_h += self.results[epsilon][interval].tolist()
+        for interval in self.intervals:
+            # absolute deviation percentage
+            errors = (
+                np.abs( self.results[0][interval] - self.results[1e-3][interval] )
+            )
+            
+            all_errors += errors.tolist()
         
-        min_h = np.min(all_h)
-        max_h = np.max(all_h)
+        min_error = np.min(all_errors)
+        max_error = np.max(all_errors)
         
         levels = 20
         
-        dh = (max_h - min_h) / levels
+        d_error = (max_error - min_error) / levels
         
-        h_levels = [ min_h + dh * i for i in range(levels + 1) ]
+        error_levels = [ min_error + d_error * i for i in range(levels + 1) ]
         
-        contourset_u0  = axs[0, 0].contourf(self.X, self.Y, self.results[0]["0 s"],     levels=levels) # for legend without negative depth
-        contourset_u6  = axs[2, 0].contourf(self.X, self.Y, self.results[0]["6 s"],     levels=h_levels)
-        contourset_u12 = axs[4, 0].contourf(self.X, self.Y, self.results[0]["12 s"],    levels=h_levels)
-        contourset_a0  = axs[0, 1].contourf(self.X, self.Y, self.results[1e-3]["0 s"],  levels=h_levels)
-        contourset_a6  = axs[2, 1].contourf(self.X, self.Y, self.results[1e-3]["6 s"],  levels=h_levels)
-        contourset_a12 = axs[4, 1].contourf(self.X, self.Y, self.results[1e-3]["12 s"], levels=h_levels)
+        err_0  = remove_nans_and_infs( np.abs( self.results[0]["0 s"]  - self.results[1e-3]["0 s"]  ) )
+        err_6  = remove_nans_and_infs( np.abs( self.results[0]["6 s"]  - self.results[1e-3]["6 s"]  ) )
+        err_12 = remove_nans_and_infs( np.abs( self.results[0]["12 s"] - self.results[1e-3]["12 s"] ) )
         
-        axs[0, 0].set_title("GPU-DG2"     if solver == "mw" else "GPU-FV1")
-        axs[0, 1].set_title( ("GPU-MWDG2, " if solver == "mw" else "GPU-HWFV1, ") + r"$L = 8, \, \epsilon = 10^{-3}$" )
+        contourset_0  = axs[0].contourf(self.X, self.Y, err_0,  levels=error_levels) # for legend without negative depth
+        contourset_6  = axs[2].contourf(self.X, self.Y, err_6,  levels=error_levels)
+        contourset_12 = axs[4].contourf(self.X, self.Y, err_12, levels=error_levels)
         
         # get axis layout (subplots) of the figure
-        gs = axs[0, 0].get_gridspec()
+        gs = axs[0].get_gridspec()
         
         # remove bottom row subplots (axes)
-        for ax in axs[1, 0:]: ax.remove()
-        for ax in axs[3, 0:]: ax.remove()
-        for ax in axs[5, 0:]: ax.remove()
-        for ax in axs[6, 0:]: ax.remove()
+        axs[1].remove()
+        axs[3].remove()
+        axs[5].remove()
+        axs[6].remove()
         
         # add subplots for time stamps via set_title
-        ax_0s   = fig.add_subplot(gs[1, 0:]); ax_0s.axis("off");  ax_0s.set_title(r"$t = 0 \, s$")
-        ax_6s   = fig.add_subplot(gs[3, 0:]); ax_6s.axis("off");  ax_6s.set_title(r"$t = 6 \, s$")
-        ax_12s  = fig.add_subplot(gs[5, 0:]); ax_12s.axis("off"); ax_12s.set_title(r"$t = 12 \, s$")
+        ax_0s   = fig.add_subplot(gs[1]); ax_0s.axis("off");  ax_0s.set_title(r"$t = 0 \, s$")
+        ax_6s   = fig.add_subplot(gs[3]); ax_6s.axis("off");  ax_6s.set_title(r"$t = 6 \, s$")
+        ax_12s  = fig.add_subplot(gs[5]); ax_12s.axis("off"); ax_12s.set_title(r"$t = 12 \, s$")
         
         # add a single subplot for the bottom row subplots
-        ax_cbar = fig.add_subplot(gs[6, 0:]); 
+        ax_cbar = fig.add_subplot(gs[6]); 
         
-        colorbar = fig.colorbar(contourset_u0, cax=ax_cbar, orientation="horizontal")#, aspect=100)
-        colorbar.ax.set_xlabel(r"$h \, (m)$")
+        colorbar = fig.colorbar(contourset_12, cax=ax_cbar, orientation="horizontal")#, aspect=100)
+        colorbar.ax.set_xlabel("Error")
         
         fig.tight_layout(h_pad=0)
         
-        plt.savefig(os.path.join("results", "three-humps-depth-contours"), bbox_inches="tight")
+        plt.savefig(os.path.join("results", "three-humps-error-contours.svg"), bbox_inches="tight")
         
 if __name__ == "__main__":
     if len(sys.argv) < 2: EXIT_HELP()
@@ -194,6 +206,6 @@ if __name__ == "__main__":
     
     if solver != "hw" and solver != "mw": EXIT_HELP()
     
-    run_c_prop_tests()
+    #run_c_prop_tests()
     
     SimulationThreeConesDamBreak( solver, [0, 1e-3] ).plot()
